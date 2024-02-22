c9259b7246aa71a2e9658a0261abd63f46612da3
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2024-02-22 17:48:18 -0600

    Added more help details

--- Main/helpAdvancedRenderer.py
+++ Main/helpAdvancedRenderer.py
@@ -23,6 +23,8 @@ def addwindowElements(window):
     bulletTwo = '• Check your JSON files section'
     bulletThree = '• Restart the software'
 
+    manualText = "If you wish to add files to your section manually you can, start by opening your section JSON file as a text document. Then find the files:{} section within the JSON object and add a file:{} object. After doing this, add a filename and filepath attribute to the object and your file should show in your section. If it does not appear, use the 'Check Correct Syntax' button to ensure you have set it up correctly."
+
     #Create window elements
     l1 = Label(window, text="Advanced Help", font=("Arial", 45))
 
@@ -37,6 +39,10 @@ def addwindowElements(window):
 
     t4 = Label(window, text=bulletThree, font=("Arial", 16), wraplength=600)
 
+    l3 = Label(window, text="How to manually add files", font=("Arial", 26))
+    t5 = Label(window, text=manualText, font=("Arial", 16), wraplength=600)
+    b3 = Button(window, text="Check Correct Syntax", font=("Arial", 10), command=lambda: messagebox.showinfo("The Librarian", 'File Syntax:\n\n{"filename": Your File Name, "filepath": Your File Path}', parent=window))
+
 
     #Set colors for elements
     l1['bg'] = '#898980'
@@ -47,6 +53,9 @@ def addwindowElements(window):
     t3['bg'] = '#A9B2AC'
     b2['bg'] = '#C5DAC1'
     t4['bg'] = '#A9B2AC'
+    l3['bg'] = '#A9B2AC' 
+    t5['bg'] = '#A9B2AC'
+    b3['bg'] = '#C5DAC1'
     
 
 
@@ -59,3 +68,6 @@ def addwindowElements(window):
     t3.grid(column=0, row=5, padx=60, pady=5, sticky='w')
     b2.grid(column=0, row=6, padx=60, pady=0)
     t4.grid(column=0, row=7, padx=60, pady=5, sticky='w')
+    l3.grid(column=0, row=8, padx=60, pady=5)
+    t5.grid(column=0, row=9, padx=60, pady=5)
+    b3.grid(column=0, row=10, padx=60, pady=0)
